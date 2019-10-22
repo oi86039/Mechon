561f45c5c683800f142d14f5635031bd2ec1ae8c
perfectharmonii <nsb29@njit.edu>
2019-10-22 11:12:27 -0400

    added title menu stuff

---
+++ Assets/Title Menu/buttonCommands.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonCommands : MonoBehaviour
+{
+	
+	public GameObject mainButton;
+    public GameObject mechonLogo;
+
+	public Animator anime;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	public void Settings() {
+	}
+
+	public void Exit() {
+		Application.Quit();
+	}
+	
+	public void startGame() {
+		SceneManager.LoadScene("insert scene name here");
+	}
+	
+	public void Credits() {
+		SceneManager.LoadScene("insert scene name here");
+	}
+	
+	public void pressMe() {
+        anime.SetTrigger("ifPressed");
+        mechonLogo.SetActive(false);
+	}
+
+}
