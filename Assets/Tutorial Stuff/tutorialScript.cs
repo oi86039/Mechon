05e977f9286ed1800d19d4d8fab8773b37db7bcf
Nataliesb17 <nsb29@njit.edu>
2019-11-26 11:06:40 -0500

    finished scope pop up

--- Assets/Tutorial Stuff/tutorialScript.cs
+++ Assets/Tutorial Stuff/tutorialScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class tutorialScript : MonoBehaviour
 {
     public GameObject rightTrigger;
-    public GameObject backRightTrigger;
+    //public GameObject backRightTrigger;
     public GameObject aimForHead;
     public GameObject scopes;
 
@@ -24,45 +24,42 @@ public class tutorialScript : MonoBehaviour
             Destroy(other);
         }
 
-        if (other.tag == "shooting")
-        {
-            backRightTrigger.SetActive(true);
-            Destroy(other);
-        }
+        //if (other.tag == "shooting")
+        //{
+        //    backRightTrigger.SetActive(true);
+        //    Destroy(other);
+        //}
 
         if (other.tag == "aiming")
         {
             aimForHead.SetActive(true);
             Destroy(other);
         }
-
-        if (other.tag == "scope")
-        {
-            scopes.SetActive(true);
-            Destroy(other);
-        }
-       
     }
 
     private void Update()
     {
+        //pickup gun
         float trigger = OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger);
         if (trigger > 0)
         {
             rightTrigger.SetActive(false);
+            scopes.SetActive(true);
         }
 
+        //shooting the gun/aiming for the head
         float backTrigger = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
         if (backTrigger > 0)
         {
-            backRightTrigger.SetActive(false);
             aimForHead.SetActive(false);
         }
 
+        //activating your scope
         bool buttonB = OVRInput.Get(OVRInput.Button.Two);
         if (buttonB == true)
         {
             scopes.SetActive(false);
+            Destroy(scopes);
         }
     }
 }
